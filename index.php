bcdf0cf15c101d171a3ebe15320716ece6a6d90b
LuckordLabtop <jorarellanoz@gmail.com>
2024-05-02 18:01:14 -0500

    Inicio

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="header">
+        <h1>Titulo principal</h1>   
+        <div id="nav">
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Nosotros</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </div>
+    </div>  
+           
+    <div id="section">
+        <h1>Sub titulo</h1>
+    </div>
+</body>
+</html>
