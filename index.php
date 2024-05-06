9f5682b61d9b026f27b25fc8d6a349fd8bb5bb84
LuckordLabtop <jorarellanoz@gmail.com>
2024-05-06 17:15:22 -0500

    Resolviendo

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilo.css">
     <title>Document</title>
 </head>
+
 <body>
     <div id="header">
         <div id="titulo">
-            <h1>PHP SOLUCIONES</h1>   
+            <h1>PHP SOLUCIONES</h1>
+        </div>
+        <div class="barra-movil">
+            <a href="">
+                <div class="sub-barra"></div>
+                <div class="sub-barra"></div>
+                <div class="sub-barra"></div>
+            </a>
         </div>
         <div id="nav">
             <ul>
@@ -18,6 +27,7 @@
                 <li><a href="">Contacto</a></li>
             </ul>
         </div>
+<<<<<<< HEAD
     </div>  
 <<<<<<< HEAD
     <!-- Sección -->
@@ -28,5 +38,17 @@
            
    
 >>>>>>> c522bfbd57600549b1034c594d3f7d61b4e9977d
+=======
+    </div>
+    <div class="barra-movil">
+        <a href="">
+            <div class="sub-barra"></div>
+            <div class="sub-barra"></div>
+            <div class="sub-barra"></div>
+        </a>
+    </div>
+
+>>>>>>> dbf973b (Documentos laptop)
 </body>
+
 </html>
